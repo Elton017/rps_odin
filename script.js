8619185c46d15dcebbbd5aaa24b8f1e9e22c0f7f
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-03 19:32:01 +1030

    playround function modified and playgame function added

--- script.js
+++ script.js
@@ -17,44 +17,73 @@ function getHumanChoice() {
     return prompt("Enter Rock, Paper or Scissors").toLowerCase()
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanSelection, computerSelection) {
+function playRound(humanScore, computerScore) {
     humanSelection = getHumanChoice()
     computerSelection = getComputerChoice()
 
     if (computerSelection == "rock") {
         if (humanSelection == "scissors") {
-            computerScore += 1
             console.log(`You picked ${humanSelection} vs ${computerSelection}, you lose! Your score: ${humanScore}, Computer score: ${computerScore}`)
+            return "loss"
         }
         else if (humanSelection == "Paper") {
-            humanScore += 1
             console.log(`You picked ${humanSelection} vs ${computerSelection}, you win! Your score: ${humanScore}, Computer score: ${computerScore}`)
+            return "win"
         }
     }
     else if (computerSelection == "paper") {
         if (humanSelection == "scissors") {
-            humanScore += 1
             console.log(`You picked ${humanSelection} vs ${computerSelection}, you win! Your score: ${humanScore}, Computer score: ${computerScore}`)
+            return "win"
         }
         else if (humanSelection == "rock") {
-            computerScore += 1
             console.log(`You picked ${humanSelection} vs ${computerSelection}, you lose! Your score: ${humanScore}, Computer score: ${computerScore}`)
+            return "loss"
         }
     }
     else if (computerSelection == "scissors") {
         if (humanSelection == "rock") {
-            humanScore += 1
             console.log(`You picked ${humanSelection} vs ${computerSelection}, you win! Your score: ${humanScore}, Computer score: ${computerScore}`)
+            return "win"
         }
         else if (humanSelection == "paper") {
-            computerScore += 1
             console.log(`You picked ${humanSelection} vs ${computerSelection}, you lose! Your score: ${humanScore}, Computer score: ${computerScore}`)
+            return "loss"
         }
     }
-    else if (computerSelection == humanSelection)
-        console.log(`You've both picked ${computerSelection}, it's a draw! Your score: ${humanScore}, Computer score: ${computerScore}`)
-
+    else if (computerSelection == humanSelection) {
+        console.log(`You've both picked ${computerSelection}, it's a draw!`)
+        return "draw"
+    }
+    else {
+        console.log("Invalid choice, please try again (Rock, Paper or Scissors)")
+        return "draw"
+    }
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        if (result == "win") {
+            humanScore += 1
+        }
+        else if (result == "loss") {
+            computerScore += 1
+        }
+        else if (result == "draw") {
+            i -= 1
+        }
+        console.log(`Your score: ${humanScore}, Computer score: ${computerScore}`)
+    }
+    if (humanScore > computerScore){
+        console.log(`You win with a score of ${humanScore} to ${computerScore}`)
+    }
+    else {
+        console.log(`You lose with a score of ${humanScore} to ${computerScore}`)
+    }
+}
+
+playGame()
