17b14f457e2ede831b2bb73d6d2fdc642a3b15d6
Elton017 <141003173+Elton017@users.noreply.github.com>
2024-11-03 19:13:53 +1030

    playround function started

--- script.js
+++ script.js
@@ -3,26 +3,58 @@
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3);
     if (randomNum == 1) {
-        return "Rock"
+        return "rock"
     }
     else if (randomNum == 2) {
-        return "Scissors"
+        return "scissors"
     }
     else if (randomNum == 0) {
-        return "Paper"
+        return "paper"
     }
 }
 
-let humanChoice = prompt("Enter 1 for Rock, 2 for Paper or 3 for Scissors")
-
 function getHumanChoice() {
-    if (humanChoice == 1){
-        return "Rock"
+    return prompt("Enter Rock, Paper or Scissors").toLowerCase()
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanSelection, computerSelection) {
+    humanSelection = getHumanChoice()
+    computerSelection = getComputerChoice()
+
+    if (computerSelection == "rock") {
+        if (humanSelection == "scissors") {
+            computerScore += 1
+            console.log(`You picked ${humanSelection} vs ${computerSelection}, you lose! Your score: ${humanScore}, Computer score: ${computerScore}`)
+        }
+        else if (humanSelection == "Paper") {
+            humanScore += 1
+            console.log(`You picked ${humanSelection} vs ${computerSelection}, you win! Your score: ${humanScore}, Computer score: ${computerScore}`)
+        }
     }
-    else if (humanChoice == 2) {
-        return "Paper"
+    else if (computerSelection == "paper") {
+        if (humanSelection == "scissors") {
+            humanScore += 1
+            console.log(`You picked ${humanSelection} vs ${computerSelection}, you win! Your score: ${humanScore}, Computer score: ${computerScore}`)
+        }
+        else if (humanSelection == "rock") {
+            computerScore += 1
+            console.log(`You picked ${humanSelection} vs ${computerSelection}, you lose! Your score: ${humanScore}, Computer score: ${computerScore}`)
+        }
     }
-    else if (humanChoice == 3) {
-        return "Scissors"
+    else if (computerSelection == "scissors") {
+        if (humanSelection == "rock") {
+            humanScore += 1
+            console.log(`You picked ${humanSelection} vs ${computerSelection}, you win! Your score: ${humanScore}, Computer score: ${computerScore}`)
+        }
+        else if (humanSelection == "paper") {
+            computerScore += 1
+            console.log(`You picked ${humanSelection} vs ${computerSelection}, you lose! Your score: ${humanScore}, Computer score: ${computerScore}`)
+        }
     }
+    else if (computerSelection == humanSelection)
+        console.log(`You've both picked ${computerSelection}, it's a draw! Your score: ${humanScore}, Computer score: ${computerScore}`)
+
 }
